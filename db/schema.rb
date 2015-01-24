b87b3ade133ba1584cd4aa6e13748d1ac2c645c8
Cody <wcodynorman@gmail.com>
2015-01-24 17:14:42 -0500

    set up for users avatar and overwrite devise controllers for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124212337) do
+ActiveRecord::Schema.define(version: 20150124213955) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "description",        limit: 255
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20150124212337) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name",                   limit: 255
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
